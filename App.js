b2907bb6e9ae552b5f765a4cfb0ec1092fda7042
connorpmullins <changeandadapt@gmail.com>
2019-01-17 09:54:20 -0800

    initial app layout constructed

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View } from 'react-native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerSide}>
+        <MaterialCommunityIcons name={"chevron-left"} size={45} color={"#fff"} onPress={null}/>
+      </View>
+      <View style={styles.headerCenter}>
+        <Text style={styles.textHeader}>Shipping and Pickup</Text>
+      </View>
+      <View style={styles.headerSide}/>
+    </View>
+  )
+}
+
+/* NOTES:
+  .Double SafeAreaView allows different colors for top and bottom of phone on iphoneX
+*/
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <SafeAreaView style={styles.safeTop} />
+        <SafeAreaView>
+
+
+          <StatusBar barStyle={"light-content"} />
+          <Header />
+          <ScrollView style={styles.body}>
+            {/*Switch between Shipping and Pickup*/}
+            <View>
+            </View>
+
+            {/*See Pic of Chair*/}
+            <View>
+            </View>
+
+            {/*Main Module (padding === outerPadding)*/}
+            <View>
+              {/*Payment Info*/}
+              <View>
+                {/*Main Payment Info*/}
+                <View></View>
+                {/*Est Total View*/}
+                <View></View>
+
+                {/*Expansion Module 1*/}
+                <View></View>
+
+                {/*Expansion Module 2*/}
+                <View></View>
+              </View>
+            </View>
+
+
+
+          </ScrollView>
+        </SafeAreaView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
+    backgroundColor: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    backgroundColor: "#00008B",
+    flexDirection: "row",
+    flexShrink: 1,
+    justifyContent: "space-between",
+    minHeight: 47,
+  },
+  headerCenter: {
+    flex: 3,
+    justifyContent: "center",
+  },
+  headerSide: {
+    flex: 1,
+  },
+  safeTop: {
+    backgroundColor: "#00008B",
+    flex: 0,
+  },
+  textHeader: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
--- README.md
+++ README.md
@@ -11,3 +11,24 @@
 
 * From Snack :
   * we'll see. Not sure yet.
+
+
+## Requirements
+* Key UI elements:
+  * The “Pickup savings” tooltip
+  * The “See Item details” expander
+  * The “Apply Promo code” expander
+  * The user should be able to enter a promo code “DISCOUNT” and on applying it, see a 10% discount reflect in the purchase summary. Use Redux for this.
+  * Please refrain from using existing react components out there to help building this (bootstrap etc.). For the pricing data, you can hard code it to an object/json and read from it.
+  * If using React web - build your UI on CodeSandbox: https://codesandbox.io/s/new and share the URL when ready
+  * Feel free to use your own workflow too if easier and upload your project files to a GitHub repo along with instructions for loading it up and testing on my end.
+  * If using React native – Feel free to use your own workflow and share the Github url for the project along with instructions for loading it up and testing on my end.
+  * I have heard https://snack.expo.io/ is another good option too (similar to codesandbox)
+  * Reusability of component is a key factor to consider. You’ll be evaluated based on code correctness and quality.
+  * You can reproduce this interface by going to walmart.com and adding this chair to Cart and going to Checkout page. You don’t have to go through with the order to see this UI.
+  * If you choose “Pickup” option in Checkout, you’ll see the “Pickup Savings” tooltip on the right as well.
+  * For the “See Item details” item image - feel free to use the images on the Walmart.com (or) any placeholder image.
+  * Clicking on “See Item Details” toggles open/close all the items’ details (not individually)
+* Bonus Points
+  * If the UI is responsive and works fine across different resolutions (mobile/desktop - scaling by % of page width). You can ignore this if using React Native.
+  * If there are unit tests for components with interactions.
